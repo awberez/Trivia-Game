ecf4f8ef50968c35591b51c72cdba88249e049f3
moltenshark <alex@berez.com>
2017-10-14 12:34:18 -0500

    can no longer cheat by inspecting answer classes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,184 +3,94 @@ $(function(){
     var qArr = [
     
         {question: 'What dinosaur name means "fast thief?"',
-        answer1: {class: "correctAnswer", content: "Velociraptor"},
-        answer2: {class: "wrongAnswer", content: "Pteronodon"},
-        answer3: {class: "wrongAnswer", content: "Nanotyrannus"},
-        answer4: {class: "wrongAnswer", content: "Sinocalliopteryx"},},
+        answer1: "Velociraptor", answer2: "Pteronodon", answer3: "Nanotyrannus", answer4: "Sinocalliopteryx"},
 
         {question: "What dinosaur was, for many years, mistakenly called a Brontosaurus?",
-        answer1: {class: "correctAnswer", content: "Apatosaurus"},
-        answer2: {class: "wrongAnswer", content: "Rigidosaurus"},
-        answer3: {class: "wrongAnswer", content: "Brachiosaurus"},
-        answer4: {class: "wrongAnswer", content: "Arcanosaurus"},},
+        answer1: "Apatosaurus", answer2: "Rigidosaurus", answer3: "Brachiosaurus", answer4: "Arcanosaurus"},
 
         {question: "Which dinosaur had fifteen horns?",
-        answer1: {class: "correctAnswer", content: "Kosmoceratops"},
-        answer2: {class: "wrongAnswer", content: "Pachyrhinosaurus"},
-        answer3: {class: "wrongAnswer", content: "Stryacosaurus"},
-        answer4: {class: "wrongAnswer", content: "Diabloceratops"},},
+        answer1: "Kosmoceratops", answer2: "Pachyrhinosaurus", answer3: "Stryacosaurus", answer4: "Diabloceratops"},
 
         {question: "Which of the following dinosaurs was toothless?",
-        answer1: {class: "correctAnswer", content: "Oviraptor"},
-        answer2: {class: "wrongAnswer", content: "Diplodocus"},
-        answer3: {class: "wrongAnswer", content: "Compsognathus"},
-        answer4: {class: "wrongAnswer", content: "Apatosaurus"},},
+        answer1: "Oviraptor", answer2: "Diplodocus", answer3: "Compsognathus", answer4: "Apatosaurus"},
 
         {question: "What close relative of the Velociraptor could grow up to 7 meters (20 feet) long and weighed almost a ton?",
-        answer1: {class: "correctAnswer", content: "Utahraptor"},
-        answer2: {class: "wrongAnswer", content: "Nevadaraptor"},
-        answer3: {class: "wrongAnswer", content: "Ohioraptor"},
-        answer4: {class: "wrongAnswer", content: "Texasraptor"},},
+        answer1: "Utahraptor", answer2: "Nevadaraptor", answer3: "Ohioraptor", answer4: "Texasraptor"},
 
         {question: "What was the first dinosaur to be discovered?",
-        answer1: {class: "correctAnswer", content: "Megalosaurus"},
-        answer2: {class: "wrongAnswer", content: "Brontosaurus"},
-        answer3: {class: "wrongAnswer", content: "Tyrannosaurus Rex"},
-        answer4: {class: "wrongAnswer", content: "Giganotosaurus"},},
+        answer1: "Megalosaurus", answer2: "Brontosaurus", answer3: "Tyrannosaurus Rex", answer4: "Giganotosaurus"},
 
         {question: "What dinosaur is believed to have weighed up to 110 tons?",
-        answer1: {class: "correctAnswer", content: "Argentinosaurus"},
-        answer2: {class: "wrongAnswer", content: "Perusaurus"},
-        answer3: {class: "wrongAnswer", content: "Columbiosaurus"},
-        answer4: {class: "wrongAnswer", content: "Brazilosaurus"},},
+        answer1: "Argentinosaurus", answer2: "Perusaurus", answer3: "Columbiosaurus", answer4: "Brazilosaurus"},
 
         {question: "During what geologic period did dinosaurs first appear?",
-        answer1: {class: "correctAnswer", content: "Triassic"},
-        answer2: {class: "wrongAnswer", content: "Cretaceous"},
-        answer3: {class: "wrongAnswer", content: "Cambrian"},
-        answer4: {class: "wrongAnswer", content: "Jurassic"},},
+        answer1: "Triassic", answer2: "Cretaceous", answer3: "Cambrian", answer4: "Jurassic"},
 
         {question: "Which of these dinosaurs was a meat eater?",
-        answer1: {class: "correctAnswer", content: "Dromaeosaurus"},
-        answer2: {class: "wrongAnswer", content: "Stegosaurus"},
-        answer3: {class: "wrongAnswer", content: "Triceratops"},
-        answer4: {class: "wrongAnswer", content: "Angulomastacator"},},
+        answer1: "Dromaeosaurus", answer2: "Stegosaurus", answer3: "Triceratops", answer4: "Angulomastacator"},
 
         {question: "What is the nickname of the most complete T-rex specimen ever discovered?",
-        answer1: {class: "correctAnswer", content: "Sue"},
-        answer2: {class: "wrongAnswer", content: "May"},
-        answer3: {class: "wrongAnswer", content: "Eve"},
-        answer4: {class: "wrongAnswer", content: "Ann"},},
+        answer1: "Sue", answer2: "May", answer3: "Eve", answer4: "Ann"},
 
         {question: "Which extinction event wiped out the dinosaurs?",
-        answer1: {class: "correctAnswer", content: "Cretaceous-Paleogene"},
-        answer2: {class: "wrongAnswer", content: "Ordovician-Silurian"},
-        answer3: {class: "wrongAnswer", content: "Permian"},
-        answer4: {class: "wrongAnswer", content: "Triassic-Jurassic"},},
+        answer1: "Cretaceous-Paleogene", answer2: "Ordovician-Silurian", answer3: "Permian", answer4: "Triassic-Jurassic"},
 
         {question: "What is the official state dinosaur of Colorado?",
-        answer1: {class: "correctAnswer", content: "Stegosaurus"},
-        answer2: {class: "wrongAnswer", content: "Allosaurus"},
-        answer3: {class: "wrongAnswer", content: "Triceratops"},
-        answer4: {class: "wrongAnswer", content: "Velociraptor"},},
+        answer1: "Stegosaurus", answer2: "Allosaurus", answer3: "Triceratops", answer4: "Velociraptor"},
 
         {question: "What was the first non-carnivorous theropod fossil to be discovered?",
-        answer1: {class: "correctAnswer", content: "Therizinosaur"},
-        answer2: {class: "wrongAnswer", content: "Baronyx"},
-        answer3: {class: "wrongAnswer", content: "Deinonychus"},
-        answer4: {class: "wrongAnswer", content: "Tarbosaurus"},},
+        answer1: "Therizinosaur", answer2: "Baronyx", answer3: "Deinonychus", answer4: "Tarbosaurus"},
 
         {question: "In what country were dinosaur fossils thought to be dragon bones?",
-        answer1: {class: "correctAnswer", content: "China"},
-        answer2: {class: "wrongAnswer", content: "South Africa"},
-        answer3: {class: "wrongAnswer", content: "Brazil"},
-        answer4: {class: "wrongAnswer", content: "Saudi Arabia"},},
+        answer1: "China", answer2: "South Africa", answer3: "Brazil", answer4: "Saudi Arabia"},
 
         {question: "How many wings did a Microraptor have?",
-        answer1: {class: "correctAnswer", content: "Four"},
-        answer2: {class: "wrongAnswer", content: "Two"},
-        answer3: {class: "wrongAnswer", content: "Six"},
-        answer4: {class: "wrongAnswer", content: "Three"},},
+        answer1: "Four", answer2: "Two", answer3: "Six", answer4: "Three"},
 
         {question: "Which of the following dinosaurs was the smallest?",
-        answer1: {class: "correctAnswer", content: "Compsognathus"},
-        answer2: {class: "wrongAnswer", content: "Raptor"},
-        answer3: {class: "wrongAnswer", content: "Troodon"},
-        answer4: {class: "wrongAnswer", content: "Dilophosaurus"},},
+        answer1: "Compsognathus", answer2: "Raptor", answer3: "Troodon", answer4: "Dilophosaurus"},
 
         {question: "What dinosaur fossil was originally mistaken for a type of bison?",
-        answer1: {class: "correctAnswer", content: "Triceratops"},
-        answer2: {class: "wrongAnswer", content: "Buffalosaurus"},
-        answer3: {class: "wrongAnswer", content: "Allosaurus"},
-        answer4: {class: "wrongAnswer", content: "Stegosaurus"},},
+        answer1: "Triceratops", answer2: "Buffalosaurus", answer3: "Allosaurus", answer4: "Stegosaurus"},
 
         {question: "What were the direct ancestors of the dinosaurs?",
-        answer1: {class: "correctAnswer", content: "Archosaurs"},
-        answer2: {class: "wrongAnswer", content: "Eukaryotes"},
-        answer3: {class: "wrongAnswer", content: "Sapheosaurs"},
-        answer4: {class: "wrongAnswer", content: "Silesaurs"},},
+        answer1: "Archosaurs", answer2: "Eukaryotes", answer3: "Sapheosaurs", answer4: "Silesaurs"},
 
         {question: "What was the lifespan of a T. rex?",
-        answer1: {class: "correctAnswer", content: "20-30 years"},
-        answer2: {class: "wrongAnswer", content: "100-120 years"},
-        answer3: {class: "wrongAnswer", content: "10-15 years"},
-        answer4: {class: "wrongAnswer", content: "50-60 years"},},
+        answer1: "20-30 years", answer2: "100-120 years", answer3: "10-15 years", answer4: "50-60 years"},
 
         {question: "Which dinosaur had the smallest brain for its body size?",
-        answer1: {class: "correctAnswer", content: "Stegosaurus"},
-        answer2: {class: "wrongAnswer", content: "Tyrannosaurus rex"},
-        answer3: {class: "wrongAnswer", content: "Triceratops"},
-        answer4: {class: "wrongAnswer", content: "Brachiosaurus"},},
+        answer1: "Stegosaurus", answer2: "Tyrannosaurus rex", answer3: "Triceratops", answer4: "Brachiosaurus"},
 
         {question: "The meteor that many scientists believe killed the dinosaurs struck in what modern-day country?",
-        answer1: {class: "correctAnswer", content: "Mexico"},
-        answer2: {class: "wrongAnswer", content: "Australia"},
-        answer3: {class: "wrongAnswer", content: "United States"},
-        answer4: {class: "wrongAnswer", content: "South Africa"},},
+        answer1: "Mexico", answer2: "Australia", answer3: "United States", answer4: "South Africa"},
 
         {question: 'What dinosaur name means "covered lizard?"',
-        answer1: {class: "correctAnswer", content: "Stegosaurus"},
-        answer2: {class: "wrongAnswer", content: "Triceratops"},
-        answer3: {class: "wrongAnswer", content: "Dimetrodon"},
-        answer4: {class: "wrongAnswer", content: "Ankylosaurus"},},
+        answer1: "Stegosaurus", answer2: "Triceratops", answer3: "Dimetrodon", answer4: "Ankylosaurus"},
 
         {question: "How many claws did a Velociraptor have on each hand?",
-        answer1: {class: "correctAnswer", content: "Three"},
-        answer2: {class: "wrongAnswer", content: "One"},
-        answer3: {class: "wrongAnswer", content: "Four"},
-        answer4: {class: "wrongAnswer", content: "Two"},},
+        answer1: "Three", answer2: "One", answer3: "Four", answer4: "Two"},
 
         {question: 'What does "dinosaur" mean?',
-        answer1: {class: "correctAnswer", content: "Terrible Lizard"},
-        answer2: {class: "wrongAnswer", content: "Hungry Lizard"},
-        answer3: {class: "wrongAnswer", content: "Killer Lizard"},
-        answer4: {class: "wrongAnswer", content: "Evil Lizard"},},
+        answer1: "Terrible Lizard", answer2: "Hungry Lizard", answer3: "Killer Lizard", answer4: "Evil Lizard"},
 
         {question: "At the beginning of the Triassic period, how many continents were there?",
-        answer1: {class: "correctAnswer", content: "One"},
-        answer2: {class: "wrongAnswer", content: "Three"},
-        answer3: {class: "wrongAnswer", content: "Seven"},
-        answer4: {class: "wrongAnswer", content: "Two"},},
+        answer1: "One", answer2: "Three", answer3: "Seven", answer4: "Two"},
 
         {question: "How are the two groups of dinosaurs differentiated?",
-        answer1: {class: "correctAnswer", content: "Hip Bones"},
-        answer2: {class: "wrongAnswer", content: "Eating Habits"},
-        answer3: {class: "wrongAnswer", content: "Number of Claws"},
-        answer4: {class: "wrongAnswer", content: "Method of Reproduction"},},
+        answer1: "Hip Bones", answer2: "Eating Habits", answer3: "Number of Claws", answer4: "Method of Reproduction"},
 
         {question: "How many fossils are necessary to identify a new dinosaur species?",
-        answer1: {class: "correctAnswer", content: "One"},
-        answer2: {class: "wrongAnswer", content: "Five"},
-        answer3: {class: "wrongAnswer", content: "Two"},
-        answer4: {class: "wrongAnswer", content: "Seven"},},
+        answer1: "One", answer2: "Five", answer3: "Two", answer4: "Seven"},
 
         {question: "Who discovered the first fossilized dinosaur eggs?",
-        answer1: {class: "correctAnswer", content: "Jean-Jacques Poech"},
-        answer2: {class: "wrongAnswer", content: "Mary Anning"},
-        answer3: {class: "wrongAnswer", content: "Gideon Mantell"},
-        answer4: {class: "wrongAnswer", content: "Roy Chapman Andrews"},},
+        answer1: "Jean-Jacques Poech", answer2: "Mary Anning", answer3: "Gideon Mantell", answer4: "Roy Chapman Andrews"},
 
         {question: "Which continents have dinosaur fossils been found on?",
-        answer1: {class: "correctAnswer", content: "All Seven Continents"},
-        answer2: {class: "wrongAnswer", content: "North America & South America"},
-        answer3: {class: "wrongAnswer", content: "Asia, Europe, & Africa"},
-        answer4: {class: "wrongAnswer", content: "Africa, North America, & Asia"},},
+        answer1: "All Seven Continents", answer2: "North America & South America", answer3: "Asia, Europe, & Africa", answer4: "Africa, North America, & Asia"},
 
         {question: "Where were the oldest dinosaur fossils in Canada found?",
-        answer1: {class: "correctAnswer", content: "Nova Scotia"},
-        answer2: {class: "wrongAnswer", content: "Yukon"},
-        answer3: {class: "wrongAnswer", content: "Saskatchewan"},
-        answer4: {class: "wrongAnswer", content: "Alberta"},},
+        answer1: "Nova Scotia", answer2: "Yukon", answer3: "Saskatchewan", answer4: "Alberta"},
 
     ]; //trivia questions taken from: http://www.usefultrivia.com/miscellaneous_trivia/dinosaur_trivia_index.html
 
@@ -222,9 +132,10 @@ $(function(){
         for (i = 0; i < randArr.length; i++) {
             aArr = arrRandomize(aArr);
             var qDiv = $("<div>");
-            qDiv.attr("id", "qDiv").attr("class", "panel panel-primary").append('<div class="question panel-heading"><h2>' + qArr[randArr[i]].question + '</h2></div>');
+            qDiv.attr("id", "qDiv").data('answer', qArr[randArr[i]].answer1).attr("class", "panel panel-primary")
+                .append('<div class="question panel-heading"><h2>' + qArr[randArr[i]].question + '</h2></div>');
             for (k = 0; k < aArr.length; k++) {
-                qDiv.append('<div class="' + qArr[randArr[i]][aArr[k]].class + ' panel-body answer" ><h3>' + qArr[randArr[i]][aArr[k]].content + '</h3></div>');
+                qDiv.append('<div class="panel-body answer" ><h3>' + qArr[randArr[i]][aArr[k]] + '</h3></div>');
             }
             divArr.push(qDiv);
         }
@@ -285,7 +196,7 @@ $(function(){
     }
 
     function userStats() {
-        var correctReveal = '<div class="panel-body">Correct Answer:<br><span class="statsVar answerReveal">' + $("#qDiv .correctAnswer").text() + '</span></div>';
+        var correctReveal = '<div class="panel-body">Correct Answer:<br><span class="statsVar answerReveal">' + $("#qDiv").data("answer") + '</span></div>';
         var statsDiv = $("<div>");
         statsDiv.attr("id", "stats").attr("class", "panel");
         if (correctState) {
@@ -334,7 +245,7 @@ $(function(){
     });
 
     $(document).on("click", ".answer", function () {
-        if ($(this).hasClass("correctAnswer")) {
+        if ($(this).text() == $("#qDiv").data("answer")) {
             correctState = true;
             userCorrect++;
         }
