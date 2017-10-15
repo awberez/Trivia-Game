32f507c8d6e148761bca91fa03088fe4a3b779f7
moltenshark <alex@berez.com>
2017-10-15 18:16:18 -0400

    better id name

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -129,7 +129,7 @@ $(function(){
         correctState = false, wrongState = false, timeActual = time, intervalId = setInterval(countDown, 1e3);
         $(".progress-bar").removeClass('progress-bar-primary').addClass('progress-bar-success');
         $("#qDisplay").html(divArr[qCount]).css("display", "none").fadeIn("fast");
-        location.href = "#time";
+        location.href = "#trivia-time";
     }
 
     function startGame() {
