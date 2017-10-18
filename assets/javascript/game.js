f2d1130108761a8a965aff7e9e7058332c55b2f6
moltenshark <alex@berez.com>
2017-10-18 12:02:02 -0500

    more compact

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,97 +1,66 @@
 $(function(){
     
     var qArr = [
-    
         {question: 'What dinosaur name means "fast thief?"',
-        answer: ["Velociraptor", "Pteronodon", "Nanotyrannus", "Sinocalliopteryx"]},
-
+           answer: ["Velociraptor", "Pteronodon", "Nanotyrannus", "Sinocalliopteryx"]},
         {question: "What dinosaur was, for many years, mistakenly called a Brontosaurus?",
-        answer: ["Apatosaurus", "Rigidosaurus", "Brachiosaurus", "Arcanosaurus"]},
-
+           answer: ["Apatosaurus", "Rigidosaurus", "Brachiosaurus", "Arcanosaurus"]},
         {question: "Which dinosaur had fifteen horns?",
-        answer: ["Kosmoceratops", "Pachyrhinosaurus", "Stryacosaurus", "Diabloceratops"]},
-
+           answer: ["Kosmoceratops", "Pachyrhinosaurus", "Stryacosaurus", "Diabloceratops"]},
         {question: "Which of the following dinosaurs was toothless?",
-        answer: ["Oviraptor", "Diplodocus", "Compsognathus", "Apatosaurus"]},
-
+           answer: ["Oviraptor", "Diplodocus", "Compsognathus", "Apatosaurus"]},
         {question: "What close relative of the Velociraptor could grow up to 7 meters (20 feet) long and weighed almost a ton?",
-        answer: ["Utahraptor", "Nevadaraptor", "Ohioraptor", "Texasraptor"]},
-
+           answer: ["Utahraptor", "Nevadaraptor", "Ohioraptor", "Texasraptor"]},
         {question: "What was the first dinosaur to be discovered?",
-        answer: ["Megalosaurus", "Brontosaurus", "Tyrannosaurus Rex", "Giganotosaurus"]},
-
+           answer: ["Megalosaurus", "Brontosaurus", "Tyrannosaurus Rex", "Giganotosaurus"]},
         {question: "What dinosaur is believed to have weighed up to 110 tons?",
-        answer: ["Argentinosaurus", "Perusaurus", "Columbiosaurus", "Brazilosaurus"]},
-
+           answer: ["Argentinosaurus", "Perusaurus", "Columbiosaurus", "Brazilosaurus"]},
         {question: "During what geologic period did dinosaurs first appear?",
-        answer: ["Triassic", "Cretaceous", "Cambrian", "Jurassic"]},
-
+           answer: ["Triassic", "Cretaceous", "Cambrian", "Jurassic"]},
         {question: "Which of these dinosaurs was a meat eater?",
-        answer: ["Dromaeosaurus", "Stegosaurus","Triceratops", "Angulomastacator"]},
-
+           answer: ["Dromaeosaurus", "Stegosaurus","Triceratops", "Angulomastacator"]},
         {question: "What is the nickname of the most complete T-rex specimen ever discovered?",
-        answer: ["Sue", "May", "Eve", "Ann"]},
-
+           answer: ["Sue", "May", "Eve", "Ann"]},
         {question: "Which extinction event wiped out the dinosaurs?",
-        answer: ["Cretaceous-Paleogene", "Ordovician-Silurian", "Permian", "Triassic-Jurassic"]},
-
+           answer: ["Cretaceous-Paleogene", "Ordovician-Silurian", "Permian", "Triassic-Jurassic"]},
         {question: "What is the official state dinosaur of Colorado?",
-        answer: ["Stegosaurus", "Allosaurus", "Triceratops", "Velociraptor"]},
-
+           answer: ["Stegosaurus", "Allosaurus", "Triceratops", "Velociraptor"]},
         {question: "What was the first non-carnivorous theropod fossil to be discovered?",
-        answer: ["Therizinosaur", "Baronyx", "Deinonychus", "Tarbosaurus"]},
-
+           answer: ["Therizinosaur", "Baronyx", "Deinonychus", "Tarbosaurus"]},
         {question: "In what country were dinosaur fossils thought to be dragon bones?",
-        answer: ["China", "South Africa", "Brazil", "Saudi Arabia"]},
-
+           answer: ["China", "South Africa", "Brazil", "Saudi Arabia"]},
         {question: "How many wings did a Microraptor have?",
-        answer: ["Four", "Two", "Six", "Three"]},
-
+           answer: ["Four", "Two", "Six", "Three"]},
         {question: "Which of the following dinosaurs was the smallest?",
-        answer: ["Compsognathus", "Raptor", "Troodon", "Dilophosaurus"]},
-
+           answer: ["Compsognathus", "Raptor", "Troodon", "Dilophosaurus"]},
         {question: "What dinosaur fossil was originally mistaken for a type of bison?",
-        answer: ["Triceratops", "Buffalosaurus", "Allosaurus", "Stegosaurus"]},
-
+           answer: ["Triceratops", "Buffalosaurus", "Allosaurus", "Stegosaurus"]},
         {question: "What were the direct ancestors of the dinosaurs?",
-        answer: ["Archosaurs", "Eukaryotes", "Sapheosaurs", "Silesaurs"]},
-
+           answer: ["Archosaurs", "Eukaryotes", "Sapheosaurs", "Silesaurs"]},
         {question: "What was the lifespan of a T. rex?",
-        answer: ["20-30 years", "100-120 years", "10-15 years", "50-60 years"]},
-
+           answer: ["20-30 years", "100-120 years", "10-15 years", "50-60 years"]},
         {question: "Which dinosaur had the smallest brain for its body size?",
-        answer: ["Stegosaurus", "Tyrannosaurus rex", "Triceratops", "Brachiosaurus"]},
-
+           answer: ["Stegosaurus", "Tyrannosaurus rex", "Triceratops", "Brachiosaurus"]},
         {question: "The meteor that many scientists believe killed the dinosaurs struck in what modern-day country?",
-        answer: ["Mexico", "Australia", "United States", "South Africa"]},
-
+           answer: ["Mexico", "Australia", "United States", "South Africa"]},
         {question: 'What dinosaur name means "covered lizard?"',
-        answer: ["Stegosaurus", "Triceratops", "Dimetrodon", "Ankylosaurus"]},
-
+           answer: ["Stegosaurus", "Triceratops", "Dimetrodon", "Ankylosaurus"]},
         {question: "How many claws did a Velociraptor have on each hand?",
-        answer: ["Three", "One", "Four", "Two"]},
-
+           answer: ["Three", "One", "Four", "Two"]},
         {question: 'What does "dinosaur" mean?',
-        answer: ["Terrible Lizard", "Hungry Lizard", "Killer Lizard", "Evil Lizard"]},
-
+           answer: ["Terrible Lizard", "Hungry Lizard", "Killer Lizard", "Evil Lizard"]},
         {question: "At the beginning of the Triassic period, how many continents were there?",
-        answer: ["One", "Three", "Seven", "Two"]},
-
+           answer: ["One", "Three", "Seven", "Two"]},
         {question: "How are the two groups of dinosaurs differentiated?",
-        answer: ["Hip Bones", "Eating Habits", "Number of Claws", "Method of Reproduction"]},
-
+           answer: ["Hip Bones", "Eating Habits", "Number of Claws", "Method of Reproduction"]},
         {question: "How many fossils are necessary to identify a new dinosaur species?",
-        answer: ["One", "Five", "Two", "Seven"]},
-
+           answer: ["One", "Five", "Two", "Seven"]},
         {question: "Who discovered the first fossilized dinosaur eggs?",
-        answer: ["Jean-Jacques Poech", "Mary Anning", "Gideon Mantell", "Roy Chapman Andrews"]},
-
+           answer: ["Jean-Jacques Poech", "Mary Anning", "Gideon Mantell", "Roy Chapman Andrews"]},
         {question: "Which continents have dinosaur fossils been found on?",
-        answer: ["All Seven Continents", "North America & South America", "Asia, Europe, & Africa", "Africa, North America, & Asia"]},
-
+           answer: ["All Seven Continents", "North America & South America", "Asia, Europe, & Africa", "Africa, North America, & Asia"]},
         {question: "Where were the oldest dinosaur fossils in Canada found?",
-        answer: ["Nova Scotia", "Yukon", "Saskatchewan", "Alberta"]},
-
+           answer: ["Nova Scotia", "Yukon", "Saskatchewan", "Alberta"]},
     ], //trivia questions taken from: http://www.usefultrivia.com/miscellaneous_trivia/dinosaur_trivia_index.html
 
     aArr = [0, 1, 2, 3], time = 30, highScore = 0,
@@ -164,8 +133,7 @@ $(function(){
     }
 
     function endGame() {
-        $("#stats").empty().css("display", "none").attr("class", "panel panel-info")
-            .append(`<div class="panel-heading"><h2>Final Score:<h2></div>`)
+        $("#stats").empty().css("display", "none").attr("class", "panel panel-info").append(`<div class="panel-heading"><h2>Final Score:<h2></div>`)
             .append(`<div class="panel-body statsCorrect">Correct: <span class="statsVar">${userCorrect}</span></div>`)
             .append(`<div class="panel-body statsWrong">Wrong: <span class="statsVar">${userWrong}</span></div>`)
             .append(`<div class="panel-body statsMissed">Missed: <span class="statsVar">${userMissed}</span></div>`);
